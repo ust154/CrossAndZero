3c9ab2bbd2fcabefd588dd8c3b96cf634a245b9c
ust154 <ustas1987@gmail.com>
2019-04-15 21:56:11 +0300

    created and filled 2D mass

--- src/CrossPlay.java
+++ src/CrossPlay.java
@@ -1,5 +1,11 @@
 public class CrossPlay {
     public static void main(String[] args) {
-
+        char[][] field = new char[4][4];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field.length; j++) {
+                field[i][j] = '*';
+            }
+        }
     }
 }
+
