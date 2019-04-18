83bf7eedd6cb5b5c93b8a44a63673c200ba3c18c
ust154 <46665485+ust154@users.noreply.github.com>
2019-04-18 17:40:28 +0300

    Update Players.java
    make method by player`s move on the x and y coordinate

--- src/Players.java
+++ src/Players.java
@@ -9,5 +9,16 @@ public class Players{
   
   BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
   String nameOfPlayer = read.readLine();
+  
+  //player`s move in the game
+  void moveByPlayer(){
+  System.out.println("Make your move along the X axis!")
+  BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+  int coordinateX = Integer.parseInt(reader.readLine());
     
+  System.out.println("Make your move along the Y axis!")
+  BufferedReader reader = new BufferedReader(new InputStreamReader (System.in));
+  int coordinateY = Integer.parseInt(reader.readLine());
+    
+  }
 }
