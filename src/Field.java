5d7c6c8b49b97c910f35182b78f277ff0c9a6aa3
ust154 <46665485+ust154@users.noreply.github.com>
2019-04-18 11:21:26 +0300

    Update Field.java
    created playing field
    created a method to show the field

--- src/Field.java
+++ src/Field.java
@@ -1,2 +1,16 @@
 public class Field{
+  public final int SIZE = 3;
+  //created playning field
+  char[][] field = new char[SIZE][SIZE];
+  for (int i = 0; i < SIZE; i++){
+    for (int j = 0; i < SIZE; j++){
+    field[i][j] = '.';
 }
+    //method to show the field
+    void showField(){
+    for ([] i : field){
+      for (j : i)
+    System.out.print(j);
+    }
+      System.out.println();
+    }
