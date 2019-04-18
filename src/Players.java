936fc754d0e1e90cb62a2d7edcffaa7b85fce1cb
ust154 <46665485+ust154@users.noreply.github.com>
2019-04-18 11:47:37 +0300

    Update Players.java
    created 2 new players

--- src/Players.java
+++ src/Players.java
@@ -1,2 +1,10 @@
 public class Players{
+  String name;
+  public Players (String name){
+  this.name = name;
+  }
+  
+  Players humanPlayer = new Players();
+  Players computerPlayer = new Players();
+    
 }
