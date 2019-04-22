98b9eb569594a4fdf0ef164b543a3eefc8cb6568
ust154 <ustas1987@gmail.com>
2019-04-22 21:48:50 +0300

    updated class Main. Added public static String updated method .creatPlayers

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
 
 public class Main {
+    private static String nameOfPlayer;
+
     public static void main(String[] args) throws IOException {
-        Players.helloPlayers();
+        Players.helloPlayers(nameOfPlayer);
         Players.creatPlayers();
         Field.creatField();
         Players.moveByPlayer();
--- src/Players.java
+++ src/Players.java
@@ -5,14 +5,16 @@ import java.io.InputStreamReader;
 public class Players {
     public static final String NAMEPC = "'Windows OS'";
 
-    public static void helloPlayers() {
+    public static void helloPlayers(String nameOfPlayer) {
         System.out.println("Hello, player! My name is " + NAMEPC);
         System.out.println("Now we start to play the game. Tell me your name.");
+
     }
 
     public static void creatPlayers() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String nameOfPlayer = reader.readLine();
+        System.out.println("Nice to meet you, " + nameOfPlayer + "!");
         Players humanPlayer = new Players();
         Players computerPlayer = new Players();
     }
